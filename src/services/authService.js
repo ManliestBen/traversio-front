c997626851c72b16abd7e696604da37e1d2d85eb
Ben Manley <benjamintmanley@gmail.com>
2024-06-11 10:16:32 -0500

    fix bug in resetPassword service function

--- src/services/authService.js
+++ src/services/authService.js
@@ -32,7 +32,7 @@ async function resetPassword(email) {
     const res = await fetch(`${BASE_URL}/reset-password`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(email),
+      body: JSON.stringify({email}),
     })
     await res.json()
   } catch (err) {
