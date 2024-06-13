a246589b7838daae3335c665551eb5a92d2584c4
Ben Manley <benjamintmanley@gmail.com>
2024-06-13 14:13:50 -0500

    display unread messages differently

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -3,12 +3,16 @@ import styles from './Message.module.css'
 const Message = ({message, user, profile}) => {
   return (
     <>
-      <div className={styles.message}>
+      <div className={`${styles.message} ${message.read ? '' : styles.unRead}`}>
         <h5 className={styles.toFrom}>
           {message.sender._id === user.profile ? 'To' : 'From' }:
         </h5>
-        <h5 className={styles.senderRecipient}>Recipient</h5>
-        <h5 className={styles.subject}>Subject</h5>
+        <h5 className={styles.senderRecipient}>
+        {message.sender._id === user.profile ? message.recipient.name : message.sender.name }
+        </h5>
+        <h5 className={styles.subject}>
+          {message.subject}
+        </h5>
       </div>
     </>
   )
