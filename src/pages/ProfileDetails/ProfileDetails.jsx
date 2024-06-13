d7a6eccd831989ba089d3b2a7394ff64113fbe0a
Ben Manley <benjamintmanley@gmail.com>
2024-06-13 13:00:31 -0500

    add UI for sending message

--- src/pages/ProfileDetails/ProfileDetails.jsx
+++ src/pages/ProfileDetails/ProfileDetails.jsx
@@ -4,10 +4,20 @@ import { useParams } from 'react-router-dom'
 import styles from './ProfileDetails.module.css'
 
 import { show } from '../../services/profileService'
+import { create } from '../../services/messageService'
 
 const ProfileDetails = (props) => {
   const [profile, setProfile] = useState({})
+  const [displayForm, setDisplayForm] = useState(false)
   const {profileId} = useParams()
+  const [messageSent, setMessageSent] = useState(false)
+  const [formData, setFormData] = useState({
+    subject: '',
+    content: '',
+    sender: props.user.profile,
+    recipient: profileId,
+    read: false
+  })
 
   useEffect(() => {
     const fetchProfileData = async () => {
@@ -17,6 +27,19 @@ const ProfileDetails = (props) => {
     fetchProfileData()
   }, [profileId])
 
+  const handleChange = evt => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value })
+  }
+
+  const handleSendMessage = async evt => {
+    evt.preventDefault()
+    setMessageSent(true)
+    setTimeout(() => {
+      setMessageSent(false)
+    }, 2000)
+    setDisplayForm(!displayForm)
+  }
+
   return (
     <>
       <div className={styles.container}>
@@ -24,7 +47,33 @@ const ProfileDetails = (props) => {
           <h1>This is my profile</h1>
         }
         {props.user.profile !== profile._id &&
+        <>
           <h1>This is NOT my profile</h1>
+          {messageSent && <h3>Message sent!</h3>}
+          {!displayForm && <button onClick={() => setDisplayForm(!displayForm)} className={styles.messageButton}>Send Message</button>}
+          {displayForm && <form className={styles.container}>
+            <label className={styles.label}>
+              Subject
+              <input
+                value={formData.subject}
+                name="subject"
+                onChange={handleChange}
+              />
+            </label>
+            <label className={styles.label}>
+              Message
+              <textarea
+                value={formData.content}
+                name="content"
+                cols={50}
+                rows={10}
+                onChange={handleChange}
+              />
+            </label>
+            <button onClick={handleSendMessage} className={styles.messageButton}>Send 📨</button>
+            <button onClick={() => setDisplayForm(!displayForm)}>Cancel</button>
+          </form>}
+        </>
         }
       </div>
     </>
